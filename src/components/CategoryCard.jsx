a322d65b68ab001e73eed6d447491a1d550e2b40
yinjianbin <yinjianbin@meituan.com>
2026-01-01 17:57:17 +0800

    refactor: remove unused migrate.js API - one-time KV to D1 migration script

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,6 +1,5 @@
-import { useState, useRef, useEffect } from 'react'
-import { ToolCard } from './ToolCard'
-import { useData } from '../hooks/useData'
+import {useEffect, useRef, useState} from 'react'
+import {ToolCard} from './ToolCard'
 import Sortable from 'sortablejs'
 
 export const CategoryCard = ({
